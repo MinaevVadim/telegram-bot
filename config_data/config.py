0620495420c3f758e26a3b93628e25bcd27a445d
Minaev Vadim <minaev.vad@yandex.ru>
2022-05-02 15:37:31 +0300

    Добавление точки.

--- config_data/config.py
+++ config_data/config.py
@@ -9,6 +9,6 @@ else:
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 RAPID_API_KEY = os.getenv('RAPID_API_KEY')
 DEFAULT_COMMANDS = (
-    ('start', "Запустить бота"),
-    ('help', "Вывести справку")
+    ('start', "Запустить бота."),
+    ('help', "Вывести справку.")
 )
